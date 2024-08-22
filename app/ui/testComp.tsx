a7ea40182ca354396b2c44e99b7348b1fb336838
SanaIqbal <95401511+Sanaiqbal-dev@users.noreply.github.com>
2024-08-22 16:01:31 +0500

    build error fixed

--- app/ui/testComp.tsx
+++ app/ui/testComp.tsx
@@ -5,13 +5,13 @@ const TestComp = () => {
 
     return (
     <p
-      css={css`
-        color: red;
-        border-radius: 10px;
-        &:hover {
-            color: blue;
-            }
-      `}
+      // css={css`
+      //   color: red;
+      //   border-radius: 10px;
+      //   &:hover {
+      //       color: blue;
+      //       }
+      // `}
     >
       This is a styled component using Emotion in a Next.js Client Component.
     </p>
